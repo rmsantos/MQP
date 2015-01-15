781eb20187c23cc332be0abb3254c7ecca0369b5
rmsantos <rmsantos@wpi.edu>
2015-01-15 14:30:19 -0500

    Fixed boundary issues and commented ShipMovement.cs
    The ship should now not pass through any boundaries (left, right, top,
    or bottom). The acceleration/decelerations is also functional. The
    numbers for both acceleration and boundaries need tweaks, but everything
    is in variables so it won't be difficult.

--- Unity Project/Assets/Player Ship/ShipMovement.cs
+++ Unity Project/Assets/Player Ship/ShipMovement.cs
@@ -45,13 +45,24 @@ public class ShipMovement : MonoBehaviour {
 	bool leftHeld;
 	bool rightHeld;
 	
-	//Variable to sweat the acceleration rate
+	//Variable to store the acceleration rate
 	public float accelerationRate;
+
+	//Variable to store the max acceleration
+	public float maxAcceleration;
+
+	//Variables to store boundaries
+	float top;
+	float bottom;
+	float left;
+	float right;
 
 	/* ----------------------------------------------------------------------- */
 	/* Function    : Start()
 	 *
 	 * Description : Stores the X and Y positions of the ship in global variables.
+	 * 				Initializes acceleration to 0, the held flags to false, and the
+	 * 				boundaries to their vector locations.
 	 *
 	 * Parameters  : None
 	 *
@@ -73,6 +84,12 @@ public class ShipMovement : MonoBehaviour {
 		leftHeld = false;
 		rightHeld = false;
 
+		//Initialize the boundary variables to the appropriate parametes
+		top = 6;
+		bottom = -5;
+		left = -10;
+		right = 10;
+
 	}
 	
 	/* ----------------------------------------------------------------------- */
@@ -89,83 +106,118 @@ public class ShipMovement : MonoBehaviour {
 		/* -- LOCAL VARIABLES ---------------------------------------------------- */
 		//None
 
+		//Read input from the user. Set flags to determine which buttons are being held.
+
+		//Up or 'w'
 		if(Input.GetKeyDown("w") || Input.GetKeyDown ("up"))
 		{
+			//Flag that up is held
 			upHeld = true;
 		}
 		else if(Input.GetKeyUp ("w") || Input.GetKeyUp("up"))
+		{
+			//Flag that up has been released
 			upHeld = false;
+		}
 
+		//Down or 's'
 		if(Input.GetKeyDown("s") || Input.GetKeyDown ("down"))
 		{
+			//Flag that down is held
 			downHeld = true;
 		}
 		else if(Input.GetKeyUp ("s") || Input.GetKeyUp("down"))
+		{
+			//Flag that down has been released
 			downHeld = false;
+		}
 
+		//Left or 'a'
 		if(Input.GetKeyDown("a") || Input.GetKeyDown ("left"))
 		{
+			//Flag the left is held
 			leftHeld = true;
 		}
 		else if(Input.GetKeyUp ("a") || Input.GetKeyUp("left"))
+		{
+			//Flag that left has been released
 			leftHeld = false;
+		}
 
+		//Right or 'd'
 		if(Input.GetKeyDown("d") || Input.GetKeyDown ("right"))
 		{
+			//Flag that right is held
 			rightHeld = true;
 		}
 		else if(Input.GetKeyUp ("d") || Input.GetKeyUp("right"))
+		{
+			//Flag that right has been released
 			rightHeld = false;
+		}
 
 
+
+		//Is the up key being held?
 		if(upHeld)
 		{
-			if(yAcceleration < 0.5f)
+			//If so then check if the ship is currently at maximum vertical acceleration
+			if(yAcceleration < maxAcceleration)
+			{
+				//If not then speed up
 				yAcceleration+= accelerationRate;
+			}
 
-			if((yPos + yAcceleration) < 6)
-				yPos = yPos + yAcceleration;
-			else
-				yAcceleration = 0;
+			//Adjust the ships new position
+			yPos = yPos + yAcceleration;
 
 		}
-
-		if(downHeld)
+		//Else is down being held? (Both can't be held at the same time)
+		else if(downHeld)
 		{
-			if(yAcceleration > -0.5f)
+			//If so then check if the ship is currently at maximum deceleration
+			if(yAcceleration > -maxAcceleration)
+			{
+				//If not then slow down
 				yAcceleration-= accelerationRate;
-			
-			if((yPos + yAcceleration) > -5)
-				yPos = yPos + yAcceleration;
-			else
-				yAcceleration = 0;
+			}
+
+			//Adjust the ships new position
+			yPos = yPos + yAcceleration;
 		}
 
+
+		//Is the left key being held
 		if(leftHeld)
 		{
-			if(xAcceleration > -0.5f)
+			//If so then check if the ship is currently at maximum horizontal acceleration
+			if(xAcceleration > -maxAcceleration)
+			{
+				//If not then speed up
 				xAcceleration-= accelerationRate;
+			}
 
-			if((xPos + xAcceleration) > -10)
-				xPos = xPos + xAcceleration;
-			else
-				xAcceleration = 0;
+			//Adjust the ships new position
+			xPos = xPos + xAcceleration;
 		}
-
-		if(rightHeld)
+		//Else is right being held? (Both can't be held at the same time)
+		else if(rightHeld)
 		{
-			if(xAcceleration < 0.5f)
+			//If so then check if the ships is currently at maximum horizontal deceleration
+			if(xAcceleration < maxAcceleration)
+			{
+				//If not the slow down
 				xAcceleration+= accelerationRate;
-			
-			if((xPos + xAcceleration) < 10)
-				xPos = xPos + xAcceleration;
-			else
-				xAcceleration = 0;
+			}
+
+			//Adjust the ships new position
+			xPos = xPos + xAcceleration;
 		}
 
+		//If no horizontal keys are being held
 		if(!leftHeld && !rightHeld)
 		{
-
+			//If the horizontal acceleration is not 0, then decrease until it is 0
 			if(xAcceleration > 0f)
 			{
 				xAcceleration-=accelerationRate;
@@ -174,18 +226,21 @@ public class ShipMovement : MonoBehaviour {
 			{
 				xAcceleration+=accelerationRate;
 			}
-			
+
+			//The acceleration will most likely not hit exactly after moving some. In this case
+			//we have a tolerance of accelerationRate before we reset it to 0
 			if(xAcceleration < accelerationRate && xAcceleration > -accelerationRate)
 				xAcceleration = 0;
 
-			if((xPos + xAcceleration) < 10 && (yPos + xAcceleration) > -10)
-				xPos = xPos + xAcceleration;
-			else
-				xAcceleration = 0;
+			//Update the ships new position
+			xPos = xPos + xAcceleration;
+
 		}
 
+		//If no vertical keys are being held
 		if(!upHeld && !downHeld)
 		{
+			//If the vertical acceleration is not 0, then decrease until it is 0
 			if(yAcceleration > 0f)
 			{
 				yAcceleration-=accelerationRate;
@@ -194,16 +249,40 @@ public class ShipMovement : MonoBehaviour {
 			{
 				yAcceleration+=accelerationRate;
 			}
-			
+
+			//The acceleration will most likely not hit exactly after moving some. In this case
+			//we have a tolerance of accelerationRate before we reset it to 0
 			if(yAcceleration < accelerationRate && yAcceleration > -accelerationRate)
 				yAcceleration = 0;
 
-			if((yPos + yAcceleration) < 6 && (yPos + yAcceleration) > -5)
-				yPos = yPos + yAcceleration;
-			else
-				yAcceleration = 0;
+			//Update the ships new position
+			yPos = yPos + yAcceleration;
 		}
 
+
+		//Check if the ship has moved outside of the boundaries
+		//If so then set the ships position to that boundary and reset acceleration
+		if(yPos >= top)
+		{
+			yPos = top;
+			yAcceleration = 0;
+		}
+		else if(yPos <= bottom)
+		{
+			yPos = bottom;
+			yAcceleration = 0;
+		}
+
+		if(xPos >= right)
+		{
+			xPos = right;
+			xAcceleration = 0;
+		}
+		else if(xPos <= left)
+		{
+			xPos = left;
+			xAcceleration = 0;
+		}
 
 		//Update new ship position
 		transform.position = new Vector3 (xPos, yPos, transform.position.z);
