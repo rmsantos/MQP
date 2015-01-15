9358516ee9be7bd0ba6d675519574d743c3acec9
rmsantos <rmsantos@wpi.edu>
2015-01-15 16:44:16 -0500

    Added Gunner and Bullet files for shooting
    Meep ^

---
+++ Unity Project/Assets/Player Ship/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	bool set;
+	float distance;
+
+	// Use this for initialization
+	void Start () {
+	
+		set = false;
+		distance = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonDown(0))
+		{
+			distance = Vector3.Distance (GameObject.FindGameObjectWithTag("Ship").transform.position,Input.mousePosition);
+			set = true;
+		}
+
+		if(set)
+		{
+			transform.position = new Vector3(transform.position.x+(1/distance),transform.position.y + (1/distance),transform.position.z);
+		}
+
+	}
+}
---
+++ Unity Project/Assets/Player Ship/Gunner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gunner : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			print (Input.mousePosition);
+
+			Vector3 pos = GetComponentInParent<Transform>().position;
+
+			float angle = Vector3.Angle(pos, Input.mousePosition);
+
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab,pos,Quaternion.identity);
+
+			bullet.transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+
+			bullet.transform.rotation = Quaternion.AngleAxis (angle,Vector3.forward);
+
+		}
+	
+	}
+}
