c9487b941793e9ef35ada6fea72f19318cb9f6cb
rmsantos <rmsantos@wpi.edu>
2015-01-15 13:47:55 -0500

    Added acceleration/deceleration to ShipMovement.cs
    Added button holding functionality to ShipMovement.cs to allow for
    acceleration and deceleration. Still working on having it work properly
    with the games boundaries.

--- Unity Project/Assets/Player Ship/ShipMovement.cs
+++ Unity Project/Assets/Player Ship/ShipMovement.cs
@@ -31,9 +31,22 @@ public class ShipMovement : MonoBehaviour {
 	/* -- GLOBAL VARIABLES --------------------------------------------------- */
 
 	//Will hold the X and Y position in the ship for comparison later
-	public float xPos;
-	public float yPos;
+	float xPos;
+	float yPos;
 
+	//Variable used to implement acceleration
+	//One for x and y acceleration
+	float xAcceleration;
+	float yAcceleration;
+
+	//Variables to track key holding
+	bool upHeld;
+	bool downHeld;
+	bool leftHeld;
+	bool rightHeld;
+	
+	//Variable to sweat the acceleration rate
+	public float accelerationRate;
 
 	/* ----------------------------------------------------------------------- */
 	/* Function    : Start()
@@ -49,6 +62,16 @@ public class ShipMovement : MonoBehaviour {
 		//Store X and Y positions of the ship
 		xPos = transform.position.x;
 		yPos = transform.position.y;
+
+		//Initialize acceleration to 0
+		xAcceleration = 0;
+		yAcceleration = 0;
+
+		//Initialize the button held keys. False means the button is not held
+		upHeld = false;
+		downHeld = false;
+		leftHeld = false;
+		rightHeld = false;
 
 	}
 	
@@ -66,33 +89,121 @@ public class ShipMovement : MonoBehaviour {
 		/* -- LOCAL VARIABLES ---------------------------------------------------- */
 		//None
 
-	
-		//LOLOL PRIMITIVE MOVEMENT
 		if(Input.GetKeyDown("w") || Input.GetKeyDown ("up"))
 		{
-			if(yPos < 6)
-				yPos++;
-
+			upHeld = true;
 		}
-		
+		else if(Input.GetKeyUp ("w") || Input.GetKeyUp("up"))
+			upHeld = false;
+
 		if(Input.GetKeyDown("s") || Input.GetKeyDown ("down"))
 		{
-			if(yPos > -5)
-				yPos--;
+			downHeld = true;
 		}
+		else if(Input.GetKeyUp ("s") || Input.GetKeyUp("down"))
+			downHeld = false;
 
 		if(Input.GetKeyDown("a") || Input.GetKeyDown ("left"))
 		{
-			if(xPos > -10)
-				xPos--;
+			leftHeld = true;
 		}
+		else if(Input.GetKeyUp ("a") || Input.GetKeyUp("left"))
+			leftHeld = false;
 
 		if(Input.GetKeyDown("d") || Input.GetKeyDown ("right"))
 		{
-			if(xPos < 10)
-				xPos++;
+			rightHeld = true;
+		}
+		else if(Input.GetKeyUp ("d") || Input.GetKeyUp("right"))
+			rightHeld = false;
+
+
+		if(upHeld)
+		{
+			if(yAcceleration < 0.5f)
+				yAcceleration+= accelerationRate;
+
+			if((yPos + yAcceleration) < 6)
+				yPos = yPos + yAcceleration;
+			else
+				yAcceleration = 0;
 
 		}
+
+		if(downHeld)
+		{
+			if(yAcceleration > -0.5f)
+				yAcceleration-= accelerationRate;
+			
+			if((yPos + yAcceleration) > -5)
+				yPos = yPos + yAcceleration;
+			else
+				yAcceleration = 0;
+		}
+
+		if(leftHeld)
+		{
+			if(xAcceleration > -0.5f)
+				xAcceleration-= accelerationRate;
+
+			if((xPos + xAcceleration) > -10)
+				xPos = xPos + xAcceleration;
+			else
+				xAcceleration = 0;
+		}
+
+		if(rightHeld)
+		{
+			if(xAcceleration < 0.5f)
+				xAcceleration+= accelerationRate;
+			
+			if((xPos + xAcceleration) < 10)
+				xPos = xPos + xAcceleration;
+			else
+				xAcceleration = 0;
+		}
+
+		if(!leftHeld && !rightHeld)
+		{
+
+			if(xAcceleration > 0f)
+			{
+				xAcceleration-=accelerationRate;
+			}
+			else if(xAcceleration < 0f)
+			{
+				xAcceleration+=accelerationRate;
+			}
+			
+			if(xAcceleration < accelerationRate && xAcceleration > -accelerationRate)
+				xAcceleration = 0;
+
+			if((xPos + xAcceleration) < 10 && (yPos + xAcceleration) > -10)
+				xPos = xPos + xAcceleration;
+			else
+				xAcceleration = 0;
+		}
+
+		if(!upHeld && !downHeld)
+		{
+			if(yAcceleration > 0f)
+			{
+				yAcceleration-=accelerationRate;
+			}
+			else if(yAcceleration < 0f)
+			{
+				yAcceleration+=accelerationRate;
+			}
+			
+			if(yAcceleration < accelerationRate && yAcceleration > -accelerationRate)
+				yAcceleration = 0;
+
+			if((yPos + yAcceleration) < 6 && (yPos + yAcceleration) > -5)
+				yPos = yPos + yAcceleration;
+			else
+				yAcceleration = 0;
+		}
+
 
 		//Update new ship position
 		transform.position = new Vector3 (xPos, yPos, transform.position.z);
