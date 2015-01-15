c19e6c7dd025193d084574eda3b9d022b7fe4a9a
rmsantos <rmsantos@wpi.edu>
2015-01-15 11:57:43 -0500

    Added comment templates to ShipMovement.cs
    We probably want to follow some standard of documentation for this
    project. I took the format that we used in IMGD 4000 and copied that
    over. Seemed decent, so lets use that to document our stuff.

--- Unity Project/Assets/Player Ship/ShipMovement.cs
+++ Unity Project/Assets/Player Ship/ShipMovement.cs
@@ -1,22 +1,73 @@
-﻿using UnityEngine;
+﻿/* Module      : ShipMovement.cs
+ * Author      : Ryan Santos
+ * Email       : rmsantos@wpi.edu
+ * Course      : IMGD MQP
+ *
+ * Description : This file controls the movement of the player ship.
+ *
+ * Date        : 2015/1/15
+ *
+ * History:
+ * Revision      Date          Changed By
+ * --------      ----------    ----------
+ * 01.00         2015/1/15    rmsantos
+ * 
+ * First release.
+ *
+ * (c) Copyright 2015, Worcester Polytechnic Institute.
+ */
+
+/* -- INCLUDE FILES ------------------------------------------------------ */
+
+using UnityEngine;
 using System.Collections;
+
+/* -- DATA STRUCTURES ---------------------------------------------------- */
+//None
+
 
 public class ShipMovement : MonoBehaviour {
 
+	/* -- GLOBAL VARIABLES --------------------------------------------------- */
+
+	//Will hold the X and Y position in the ship for comparison later
 	public float xPos;
 	public float yPos;
 
-	// Use this for initialization
+
+	/* ----------------------------------------------------------------------- */
+	/* Function    : Start()
+	 *
+	 * Description : Stores the X and Y positions of the ship in global variables.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
 	void Start () {
 
+		//Store X and Y positions of the ship
 		xPos = transform.position.x;
 		yPos = transform.position.y;
 
 	}
 	
-	// Update is called once per frame
+	/* ----------------------------------------------------------------------- */
+	/* Function    : Update()
+	 *
+	 * Description : Read in key inputs from the player and moves the ship accordingly.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
 	void Update () {
+		
+		/* -- LOCAL VARIABLES ---------------------------------------------------- */
+		//None
+
 	
+		//LOLOL PRIMITIVE MOVEMENT
 		if(Input.GetKeyDown("w") || Input.GetKeyDown ("up"))
 		{
 			if(yPos < 6)
@@ -43,6 +94,8 @@ public class ShipMovement : MonoBehaviour {
 
 		}
 
+		//Update new ship position
 		transform.position = new Vector3 (xPos, yPos, transform.position.z);
+
 	}
 }
