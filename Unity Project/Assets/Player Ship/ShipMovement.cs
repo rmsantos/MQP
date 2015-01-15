b206725e0dc126e81c20e79f95bf40e40cbcd97a
rmsantos <rmsantos@wpi.edu>
2015-01-15 11:32:58 -0500

    Imported some standard assets / Setup test scene
    Standard assets were imported. We can delete the ones we know we won't
    use later, but I got the ones that might be useful. I setup the scene
    with primitive backdrops and player ships. Began coding a simple
    movement .cs file for the player ship.

---
+++ Unity Project/Assets/Player Ship/ShipMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	public float xPos;
+	public float yPos;
+
+	// Use this for initialization
+	void Start () {
+
+		xPos = transform.position.x;
+		yPos = transform.position.y;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown("w") || Input.GetKeyDown ("up"))
+		{
+			if(yPos < 6)
+				yPos++;
+
+		}
+		
+		if(Input.GetKeyDown("s") || Input.GetKeyDown ("down"))
+		{
+			if(yPos > -5)
+				yPos--;
+		}
+
+		if(Input.GetKeyDown("a") || Input.GetKeyDown ("left"))
+		{
+			if(xPos > -10)
+				xPos--;
+		}
+
+		if(Input.GetKeyDown("d") || Input.GetKeyDown ("right"))
+		{
+			if(xPos < 10)
+				xPos++;
+
+		}
+
+		transform.position = new Vector3 (xPos, yPos, transform.position.z);
+	}
+}
