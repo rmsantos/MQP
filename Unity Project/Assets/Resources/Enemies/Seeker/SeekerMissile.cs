74f89faefbf9546a88212ce67e1b61e029a0e142
rmsantos <rmsantos@wpi.edu>
2015-01-20 13:38:05 -0500

    Added simple code for Seeker missile

--- Unity Project/Assets/Resources/Enemies/Seeker/SeekerMissile.cs
+++ Unity Project/Assets/Resources/Enemies/Seeker/SeekerMissile.cs
@@ -1,15 +1,103 @@
-﻿using UnityEngine;
+﻿/* Module      : SeekerMissile.cs
+ * Author      : Ryan Santos
+ * Email       : rmsantos@wpi.edu
+ * Course      : IMGD MQP
+ *
+ * Description : This file controls the behavior of the Seeker's missile
+ *
+ * Date        : 2015/1/20
+ * 
+ *
+ * (c) Copyright 2015, Worcester Polytechnic Institute.
+ */
+
+/* -- INCLUDE FILES ------------------------------------------------------ */
+using UnityEngine;
 using System.Collections;
 
-public class SeekerMissile : MonoBehaviour {
+/* -- DATA STRUCTURES ---------------------------------------------------- */
+//None
 
-	// Use this for initialization
-	void Start () {
+public class SeekerMissile : MonoBehaviour {
 	
+	/* -- GLOBAL VARIABLES --------------------------------------------------- */
+	
+	//The speed at which the bullet will move
+	public float speed;
+		
+	//Stores the boundaries of the game
+	Boundaries boundaries;
+	
+	//Player script
+	GameObject player;
+	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : Start()
+	 *
+	 * Description : Searches for the player.
+	 * 				Also stores the boundaries of the game.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
+	void Start () {
+		
+		//Pull the boundaries script from the main camera object and store it
+		boundaries = Camera.main.GetComponent<Boundaries>();
+		
+		//Search for player
+		player = GameObject.FindGameObjectWithTag ("Player");
+		
 	}
 	
-	// Update is called once per frame
+	/* ----------------------------------------------------------------------- */
+	/* Function    : Update()
+	 *
+	 * Description : Moves the bullet slowly towards the player.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
 	void Update () {
+		
+		/* -- LOCAL VARIABLES ---------------------------------------------------- */
+		
+		//follow the player
+		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+		
+		//Move to the left
+		transform.position = new Vector3 (transform.position.x - (speed * 2f), transform.position.y, transform.position.z);
+				
+		//If the enemy leaves the game space
+		//Leave some room for the enemy to fully exit the visible screen (by multiplying 1.2)
+		if (transform.position.x < (boundaries.getLeft() * 1.2))
+		{
+			//Destroy the enemy
+			Destroy (this.gameObject);
+		}
+		
+	}
 	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : OnCollisionEnter(Collision col)
+	 *
+	 * Description : Deals with collisions between the player bullets and this enemy.
+	 *
+	 * Parameters  : Collision col : The other object collided with
+	 *
+	 * Returns     : Void
+	 */
+	void OnCollisionEnter (Collision col)
+	{
+		//If this is hit by a player bullet
+		if(col.gameObject.tag == "PlayerBullet")
+		{
+			//Destroy the player bullet and this object
+			Destroy(col.gameObject);
+			Destroy (this.gameObject);
+			
+		}
 	}
 }
