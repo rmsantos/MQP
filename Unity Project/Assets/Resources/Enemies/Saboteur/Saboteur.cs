4a2dd1d5bea43e494cf1118c04d08f06d8647424
rmsantos <rmsantos@wpi.edu>
2015-01-27 19:59:50 -0500

    Saboteur enemy adjust to work as we discussed

--- Unity Project/Assets/Resources/Enemies/Saboteur/Saboteur.cs
+++ Unity Project/Assets/Resources/Enemies/Saboteur/Saboteur.cs
@@ -47,9 +47,6 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 	public GameObject scoreObject;
 	static ScoreHandler score;
 	
-	//Speed in the x direction
-	float xSpeed;
-	
 	//Player script
 	GameObject player;
 
@@ -61,7 +58,13 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 	
 	//Stores the damage the bullet does
 	public int bulletDamage;
-	
+
+	//Can the ship move vertically now?
+	bool verticalMove;
+
+	//Speed changes depending on the ships state
+	float newSpeed;
+
 	/* ----------------------------------------------------------------------- */
 	/* Function    : Start()
 	 *
@@ -85,14 +88,36 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 		
 		//Search for the ScoreHandler object for tracking score
 		score = (ScoreHandler)scoreObject.GetComponent("ScoreHandler");
-		
-		//Set the x speed to move to the left 
-		xSpeed = speed;
-		
+
+		//The ship cannot initially move vertically
+		verticalMove = false;
+
+		//Speed changes depending on the ships state
+		newSpeed = speed;
+
 		//Search for player
 		player = GameObject.FindGameObjectWithTag ("Player");
 	}
-	
+
+	/* ----------------------------------------------------------------------- */
+	/* Function    : delay()
+	 *
+	 * Description : Delays for a very small time then
+	 * 				flags the ship to move vertically.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
+	IEnumerator delay()
+	{
+		//Wait a small amount of time so that the ship doesn't look choppy
+		yield return new WaitForSeconds(0.001f);
+
+		//Allow the ship to move vertically.
+		verticalMove = true;
+	}
+
 	/* ----------------------------------------------------------------------- */
 	/* Function    : FixedUpdate()
 	 *
@@ -106,40 +131,51 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 	void FixedUpdate () {
 		
 		/* -- LOCAL VARIABLES ---------------------------------------------------- */
-		
-		//Stop the enemy when it hits the left bound of the screen
-		//We actually make the enemy move extremely slowly
-		//Because of an issue with collisions, we can't hit 
-		//it unless its moving somehow
-		if (transform.position.x > boundaries.getLeft())
-			xSpeed = 0.000001f;
 
 		//Direction the enemy should move
 		float direction = 0;
 	
-		//If the within a certain threshold of the player
-		if(player == null || (Mathf.Abs(transform.position.y-player.transform.position.y) < 0.1))
+		//If the ship can move move vertically
+		if(verticalMove)
 		{
-			//Don't move
-			direction = 0;
-		}
-		//Track the movement of the player and follow it
-		else if(player.transform.position.y > transform.position.y)
-		{
-			//Move up
-			direction =  Mathf.Abs(speed);
-		}
-		else
-		{
-			//Move down
-			direction = -Mathf.Abs (speed);
+			//If the within a certain threshold of the player
+			if(player == null || (Mathf.Abs(transform.position.y-player.transform.position.y) < 0.1))
+			{
+				//Don't move
+				direction = 0;
+			}
+			//Track the movement of the player and follow it
+			else if(player.transform.position.y > transform.position.y)
+			{
+				//Move up
+				direction =  Mathf.Abs(speed);
+			}
+			else
+			{
+				//Move down
+				direction = -Mathf.Abs (speed);
+			}
 		}
 
 		//The new position of the enemy after moving
-		Vector3 newPos = new Vector3 (transform.position.x +xSpeed, transform.position.y + direction, transform.position.z);
+		Vector3 newPos = new Vector3 (boundaries.getLeft(), transform.position.y + direction, transform.position.z);
 
-		//Apply the movement
-		transform.position = newPos;
+		//If the ship is at the left side of the screen
+		if(transform.position.x == boundaries.getLeft())
+		{
+			//If this peice of code hasn't been run yet
+			if(!verticalMove)
+			{
+				//Delay a short time then change verticalMove
+				StartCoroutine(delay());
+
+				//Also decrease the speed greatly
+				newSpeed = speed/5;
+			}
+		}
+
+		//Make the move
+		transform.position = Vector3.MoveTowards(transform.position, newPos, newSpeed);
 		
 		//If the enemy is "reloading", don't decrement the timer
 		if (!ready) {
@@ -154,9 +190,9 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 				shootTimer = reloadTime;
 			}
 		}
-		
-		//If the enemy can shoot and is in bounds
-		if(ready & boundaries.inBoundaries(transform.position,1))
+
+		//If the enemy can shoot and is in bounds and can move vertically
+		if(ready && boundaries.inBoundaries(transform.position,1.2f) && verticalMove)
 		{
 			//Flag that a bullet was shot
 			ready = false;
@@ -172,7 +208,7 @@ public class Saboteur : MonoBehaviour, BasicEnemy {
 			
 			//Set the damage of the bullet
 			simpleEnemyBullet.setDamage(bulletDamage);
-		}	
+		}
 	}
 	
 	/* ----------------------------------------------------------------------- */
