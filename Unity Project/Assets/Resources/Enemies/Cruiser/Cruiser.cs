4243f40695e0f1b377455825cef38c08ea9c6fab
rmsantos <rmsantos@wpi.edu>
2015-01-28 10:39:45 -0500

    Cruiser enemy type is finished
    Probably needs some tweaking, but mechanics are in

---
+++ Unity Project/Assets/Resources/Enemies/Cruiser/Cruiser.cs
@@ -0,0 +1,236 @@
+﻿/* Module      : Cruiser.cs
+ * Author      : Ryan Santos
+ * Email       : rmsantos@wpi.edu
+ * Course      : IMGD MQP
+ *
+ * Description : This file controls the behavior of the Cruiser
+ *
+ * Date        : 2015/1/28
+ * 
+ *
+ * (c) Copyright 2015, Worcester Polytechnic Institute.
+ */
+
+/* -- INCLUDE FILES ------------------------------------------------------ */
+using UnityEngine;
+using System.Collections;
+
+/* -- DATA STRUCTURES ---------------------------------------------------- */
+//None
+
+public class Cruiser :  MonoBehaviour, BasicEnemy {
+	
+	/* -- GLOBAL VARIABLES --------------------------------------------------- */
+	
+	//The speed at which the enemy will move
+	public float speed;
+	
+	//Is the enemy ready to shoot?
+	bool ready;
+	
+	//Counter for reloading
+	int shootTimer;
+	
+	//Time before the enemy can shoot again 
+	public int reloadTime;
+	
+	//Prefab of the enemy bullet
+	public GameObject bulletPrefab;
+	
+	//Stores the boundaries of the game
+	Boundaries boundaries;
+	
+	//Value of destroying this enemy
+	public int value;
+	
+	//ScoreHandler object to track players score
+	public GameObject scoreObject;
+	static ScoreHandler score;
+	
+	//The health of this enemy
+	public int health;
+	
+	//Stores the damage colliding with the player does
+	public int collisionDamage;
+	
+	//Stores the damage the bullet does
+	public int bulletDamage;
+
+	//The amplitude of the wave that this ship will move in
+	public int amplitude;
+
+	//Counter to determine amplitude of the wave
+	int waveCounter;
+
+	//The direction the ship is moving (on the y axis)
+	float direction;
+
+	/* ----------------------------------------------------------------------- */
+	/* Function    : Start()
+	 *
+	 * Description : Initializes the firing rate variables.
+	 * 				Also stores the boundaries of the game.
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
+	void Start () {
+		
+		//The enemy can shoot right when it spawns
+		ready = true;
+		
+		//Set the shooting timer
+		shootTimer = reloadTime;
+		
+		//Pull the boundaries script from the main camera object and store it
+		boundaries = Camera.main.GetComponent<Boundaries>();
+		
+		//Search for the ScoreHandler object for tracking score
+		score = (ScoreHandler)scoreObject.GetComponent("ScoreHandler");
+
+		//Initialize the counter to be 0
+		waveCounter = 0;
+
+		//Direction is the speed
+		direction = speed/2;
+	}
+	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : FixedUpdate()
+	 *
+	 * Description : Moves the enemy slowly to the left and vertically in a wave pattern
+	 * 				and periodically shoots bullets
+	 *
+	 * Parameters  : None
+	 *
+	 * Returns     : Void
+	 */
+	void FixedUpdate () {
+		
+		/* -- LOCAL VARIABLES ---------------------------------------------------- */
+
+		//Increment the wave counter to count until amplitude
+		waveCounter++;
+
+		//When reaching the wave amplitude, reverse the direction
+		if(waveCounter == amplitude)
+		{
+			waveCounter = 0;
+			direction = - direction;
+		}
+
+
+		//The new position of the enemy after moving
+		Vector3 newPos = new Vector3 (transform.position.x - speed, transform.position.y + direction, transform.position.z);
+		
+		//Apply the movement
+		transform.position = newPos;
+		
+		//If the enemy is "reloading", don't decrement the timer
+		if (!ready) {
+			
+			//Decrements the shoot timer
+			shootTimer--;
+			
+			//If the shoot timer has reached 0, reset it and flag that the enemy can shoot
+			if (shootTimer <= 0) {
+				
+				ready = true;
+				shootTimer = reloadTime;
+			}
+		}
+		
+		//If the enemy can shoot and is in bounds
+		if(ready & boundaries.inBoundaries(transform.position,1))
+		{
+			//Flag that a bullet was shot
+			ready = false;
+			
+			//Spawn the bullet and store it
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab,transform.position,Quaternion.identity);
+			
+			//Cast to an bullet type
+			SimpleEnemyBullet simpleEnemyBullet = (SimpleEnemyBullet)bullet.GetComponent(typeof(SimpleEnemyBullet));
+			
+			//Set the damage of the bullet
+			simpleEnemyBullet.setDamage(bulletDamage);
+		}	
+		
+		//If the enemy leaves the game space
+		//Leave some room for the enemy to fully exit the visible screen (by multiplying 1.2)
+		if (transform.position.x < (boundaries.getLeft() * 1.2))
+		{
+			//Destroy the enemy
+			Destroy (this.gameObject);
+		}
+		
+	}
+	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : OnCollisionEnter2D(Collision2D col)
+	 *
+	 * Description : Deals with collisions between the player bullets and this enemy.
+	 *
+	 * Parameters  : Collision2D col : The other object collided with
+	 *
+	 * Returns     : Void
+	 */
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		//If this is hit by a player bullet
+		if(col.gameObject.tag == "PlayerBullet")
+		{
+			//Destroy the player bullet and this object
+			Destroy(col.gameObject);
+			
+			//Get the damage the player bullet will deal
+			int damage = col.gameObject.GetComponent<Bullet>().getDamage();
+			
+			//Deal the damage to this enemy
+			takeDamage(damage);
+			
+		}
+	}
+	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : takeDamage(float damage)
+	 *
+	 * Description : Deals damage to the enemies health
+	 *
+	 * Parameters  : int damage : The damage to be dealt
+	 *
+	 * Returns     : Void
+	 */
+	public void takeDamage(int damage)
+	{
+		
+		//Subtract health from the enemy
+		health -= damage;
+		
+		//If health hits 0, then the enemy dies
+		if(health <= 0)
+		{
+			//Destroy the enemy
+			Destroy (this.gameObject);	
+			
+			//Update the players score
+			score.UpdateScore(value);
+		}
+	}
+	
+	/* ----------------------------------------------------------------------- */
+	/* Function    : getCollisionDamage()
+	 *
+	 * Description : Returns the collision damage for this enemy
+	 *
+	 * Parameters  : None.
+	 *
+	 * Returns     : int:  Collision damage
+	 */
+	public int getCollisionDamage()
+	{
+		return collisionDamage;
+	}
+	
+}
