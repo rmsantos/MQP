53f60fb0c9868e88283457a956c585010d000705
jbmorse <jbmorse@wpi.edu>
2015-02-02 13:16:03 -0500

    Things on the player ship now appear and disappear

---
+++ Unity Project/Assets/Player Ship/ShipUpgrades.cs
@@ -0,0 +1,48 @@
+﻿/* Module      : ShipUpgrades.cs
+ * Author      : Joshua Morse
+ * Email       : jbmorses@wpi.edu
+ * Course      : IMGD MQP
+ *
+ * Description : This file controls the visual upgrades of the players ship
+ *
+ * Date        : 2015/2/2
+ * 
+ *
+ * (c) Copyright 2015, Worcester Polytechnic Institute.
+ */
+
+/* -- INCLUDE FILES ------------------------------------------------------ */
+using UnityEngine;
+using System.Collections;
+
+public class ShipUpgrades : MonoBehaviour {
+
+	int MissileLevel;
+	int ShieldLevel;
+	int LaserLevel;
+	int TurretLevel;
+	
+	void Start () {
+	
+		MissileLevel = 0;
+		ShieldLevel = 0;
+		LaserLevel = 0;
+		TurretLevel = 1;
+
+		if (MissileLevel < 1) {
+			transform.Find("MissileUpgrade").renderer.enabled = false;
+		}
+		if (ShieldLevel < 1) {
+			transform.Find("ShieldUpgrade").renderer.enabled = false;
+		}
+		if (LaserLevel < 1) {
+			transform.Find("LaserUpgrade").renderer.enabled = false;
+			transform.Find("LaserUpgradeAttachment").renderer.enabled = false;
+		}
+		if (TurretLevel < 1) {
+			transform.Find("TurretUpgrade").renderer.enabled = false;
+		}
+
+	}
+	
+}
