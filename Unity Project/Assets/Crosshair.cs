15893eb11558e40d3a9e9f018c9f8e989f50af62
jbmorse <jbmorse@wpi.edu>
2015-02-23 14:25:29 -0500

    Crosshair added to game

---
+++ Unity Project/Assets/Crosshair.cs
@@ -0,0 +1,25 @@
+﻿/* Module      : Crosshair.cs
+ * Author      : Josh Morse
+ * Email       : jbmorse@wpi.edu
+ * Course      : IMGD MQP
+ *
+ * Description : This file controls the behavior of the crosshair
+ *
+ * Date        : 2015/2/23
+ * 
+ *
+ * (c) Copyright 2015, Worcester Polytechnic Institute.
+ */
+
+/* -- INCLUDE FILES ------------------------------------------------------ */
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	
+	void FixedUpdate () {
+		transform.position = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+	}
+
+}
